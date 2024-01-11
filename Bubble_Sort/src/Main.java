1544dfe240f11fcc2964a5e4ba7d42463b3240dc
Kerem <kerem.ozcan2004@gmail.com>
2024-01-11 20:51:34 +0300

    Bubble Sort Code Implemented.

---
+++ Bubble_Sort/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {23, 15, -44, 6, 54, 12, -5};
+
+        int i;
+
+        for(i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+
+        for(int lastUnsortedIndex = array.length-1; lastUnsortedIndex > 1; lastUnsortedIndex--){
+            for(i = 0; i < lastUnsortedIndex; i++){
+                if(array[i] > array[i+1]){
+                    swap(array, i, i+1);
+                }
+            }
+        }
+
+        for(i = 0; i < 7; i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static  void swap(int[] array, int i, int j){
+        if(i == j){
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+}
